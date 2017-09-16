0dbe8c7782e869e125a0ca69da244551e5d9252e
OmManiPemeHung <danielwraga@gmail.com>
2017-09-16 14:25:06 +0200

    first commit

---
+++ zadanie9-5.js
@@ -0,0 +1,5 @@
+var withButtonItemClass = document.getElementsByClassName('button');
+ 	for (var i = 0 ; i <= withButtonItemClass.length ; i++) {
+		withButtonItemClass[i].innerText = "Napis" + i;
+	}
+
